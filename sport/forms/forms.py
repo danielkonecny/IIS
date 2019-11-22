b16b0f7c91cd4b049d2794d141679471bfffe07a
RouSkiSroup <jerabek.rou@gmail.com>
2019-11-22 17:24:41 +0100

    Pridavani sponsoru do turnaje
    Done TODO:
    pridat sponzora do turnaje

--- sport/forms/forms.py
+++ sport/forms/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from teams.models import Team
 from django.forms import ModelForm
 from tournaments.models import Tournament
+from sponsors.models import Sponsor
 
 # formular pridani teamu do turnaje
 class AddTeamForm(forms.Form):
@@ -15,6 +16,18 @@ class AddTeamForm(forms.Form):
         if len(t):
             self.fields['teams'].queryset = t # pristup pres self.fields do vsech prvku formulare POUZE!!!!!
             self.fields['teams'].label = 'Select team:'
+
+class AddSponsorForm(forms.Form):
+
+    sponsors = forms.ModelChoiceField(queryset=Sponsor.objects.all()) # tohle je povinnej prvek formulare, OPRAVIT NA NECO ROZUMNYHO
+
+    def __init__(self,*args,**kwargs): # custom konstrukce formulare
+        s = kwargs.pop('s') # vytahni z argumentu aktualni request
+        super(AddSponsorForm,self).__init__(*args,**kwargs) # defualtni konstruktor
+
+        if len(s):
+            self.fields['sponsors'].queryset = s # pristup pres self.fields do vsech prvku formulare POUZE!!!!!
+            self.fields['sponsors'].label = 'Select sponsor:'
 
 class CreateTeam(ModelForm):
     class Meta:
--- sport/tournaments/views.py
+++ sport/tournaments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Tournament
 from teams.models import Team
+from sponsors.models import Sponsor
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Q
@@ -8,6 +9,7 @@ from django.core.paginator import Paginator
 
 from func.func import compare
 from forms.forms import AddTeamForm
+from forms.forms import AddSponsorForm
 
 def single(request,id):
 
@@ -17,12 +19,17 @@ def single(request,id):
     rozhodci = tournament.rozhodci.all()
     sponsors = tournament.sponsors.all()
 
-    add_new = None # formular na pridani tymu do turnaje
+    aviable_sponsors = Sponsor.objects.all()
+    aviable_sponsors = aviable_sponsors.exclude(tour_sponsors__in=[tournament])
+
+    add_new_team = None # formular na pridani tymu do turnaje
+    add_new_sponsor = None  # formular na pridani tymu do turnaje
 
     # vyber vsechny tymy ktere user managuje
     managing_teams = Team.objects.filter(managers=request.user)
     available_teams = None
-    permitted_add = False
+    permitted_add_team = False
+    permitted_add_sponsor = False
     if not tournament.started:
         available_teams = managing_teams.exclude(tour_teams__in=[tournament])
         available_teams = available_teams.exclude(tour_requests_teams__in=[tournament])
@@ -30,16 +37,30 @@ def single(request,id):
 
     # ZACINA VALIDACE FORMULARE NA PRIDANI TYMU DO TURNAJE    
     if request.method == 'POST':
-        add_new = AddTeamForm(request.POST,t=available_teams)
-        if add_new.is_valid():
-            team = add_new.cleaned_data['teams']
+        add_new_team = AddTeamForm(request.POST,t=available_teams)
+        if add_new_team.is_valid():
+            team = add_new_team.cleaned_data['teams']
             tournament.requests_teams.add(team)
         
             return redirect('forms:profile')
+
+    # ZACINA VALIDACE FORMULARE NA PRIDANI SPONSORA DO TURNAJE
+    if request.method == 'POST':
+        add_new_sponsor = AddSponsorForm(request.POST, s=aviable_sponsors)
+        if add_new_sponsor.is_valid():
+            sponsors = add_new_sponsor.cleaned_data['sponsors']
+            tournament.sponsors.add(sponsors)
+
+            return redirect('forms:profile')
+
         
     if len(available_teams):
-        add_new = AddTeamForm(t=available_teams) # vytvor formular na pridani tymu do turnaje, az tedka
-        permitted_add = True
+        add_new_team = AddTeamForm(t=available_teams) # vytvor formular na pridani tymu do turnaje, az tedka
+        permitted_add_team = True
+
+    if len(aviable_sponsors):
+        add_new_sponsor = AddSponsorForm(s=aviable_sponsors) # vytvor formular na pridani tymu do turnaje, az tedka
+        permitted_add_sponsor = True
 
     # vyber vsechny tymy, ve kterych je aktualni user
     his_teams = Team.objects.filter(players__in=[request.user])
@@ -47,7 +68,11 @@ def single(request,id):
     # zjisti jestli muze byt rozhodcim
     permitted = not compare(his_teams, teams)
     
-    return render(request, 'tournaments/single.html', {'tournament': tournament, 'teams':teams,'sponsors': sponsors,'rozhodci':rozhodci,'permitted':permitted,'permitted_add':permitted_add,'add_new':add_new})
+    return render(request, 'tournaments/single.html', {'tournament': tournament, 'teams': teams,'sponsors': sponsors,
+                                                       'rozhodci': rozhodci,'permitted': permitted,
+                                                       'permitted_add_team': permitted_add_team,
+                                                       'permitted_add_sponsor': permitted_add_sponsor,
+                                                       'add_new_team': add_new_team, 'add_new_sponsor': add_new_sponsor})
     
 def index(request):
     front = Tournament.objects.all()
