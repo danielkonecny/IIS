de944a86f5c7b6a7139a66c302ef4946ac0adb38
RouSkiSroup <jerabek.rou@gmail.com>
2019-11-23 17:29:16 +0100

    tried to start match

--- sport/tournaments/urls.py
+++ sport/tournaments/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'), # /tournaments/
     path('your_tournaments/',views.your_tournaments, name='your_tournaments'),
     path('<int:id>/',views.single,name='single'), # /tournaments/25/
+    re_path(r'^(?P<id>\w+)/match_schedule$', views.start_tournament, name='start_tournament'),
+    # path('<int:id>/match_schedule',views.start_tournament(),name='start_tournament'), # /tournaments/25/match_schedule
     
 ]
 
--- sport/tournaments/views.py
+++ sport/tournaments/views.py
@@ -11,10 +11,14 @@ from func.func import compare
 from forms.forms import AddTeamForm
 from forms.forms import AddSponsorForm
 
-def single(request,id):
+from django.contrib import messages
 
-    tournament = get_object_or_404(Tournament,pk=id)
-    
+from django.db.models.functions import Log
+
+
+def single(request, id):
+    tournament = get_object_or_404(Tournament, pk=id)
+
     teams = tournament.teams.all()
     rozhodci = tournament.rozhodci.all()
     sponsors = tournament.sponsors.all()
@@ -22,7 +26,7 @@ def single(request,id):
     aviable_sponsors = Sponsor.objects.all()
     aviable_sponsors = aviable_sponsors.exclude(tour_sponsors__in=[tournament])
 
-    add_new_team = None # formular na pridani tymu do turnaje
+    add_new_team = None  # formular na pridani tymu do turnaje
     add_new_sponsor = None  # formular na pridani tymu do turnaje
 
     # vyber vsechny tymy ktere user managuje
@@ -38,14 +42,14 @@ def single(request,id):
 
     # ZACINA VALIDACE FORMULARE NA PRIDANI TYMU DO TURNAJE    
     if request.method == 'POST':
-        add_new_team = AddTeamForm(request.POST,t=available_teams)
+        add_new_team = AddTeamForm(request.POST, t=available_teams)
         if add_new_team.is_valid():
             team = add_new_team.cleaned_data['teams']
             if team.managers == user and tournament.poradatele == user:
                 tournament.teams.add(team)
             else:
                 tournament.requests_teams.add(team)
-        
+
             return redirect('forms:profile')
 
     # ZACINA VALIDACE FORMULARE NA PRIDANI SPONSORA DO TURNAJE
@@ -57,14 +61,13 @@ def single(request,id):
 
             return redirect('forms:profile')
 
-        
     if len(available_teams):
-        add_new_team = AddTeamForm(t=available_teams) # vytvor formular na pridani tymu do turnaje, az tedka
+        add_new_team = AddTeamForm(t=available_teams)  # vytvor formular na pridani tymu do turnaje, az tedka
         permitted_add_team = True
 
     if len(aviable_sponsors):
-        add_new_sponsor = AddSponsorForm(s=aviable_sponsors) # vytvor formular na pridani tymu do turnaje, az tedka
-        if(user == tournament.poradatele):
+        add_new_sponsor = AddSponsorForm(s=aviable_sponsors)  # vytvor formular na pridani tymu do turnaje, az tedka
+        if (user == tournament.poradatele):
             permitted_add_sponsor = True
         else:
             permitted_add_sponsor = False
@@ -74,23 +77,25 @@ def single(request,id):
 
     # zjisti jestli muze byt rozhodcim
     permitted = not compare(his_teams, teams)
-    
-    return render(request, 'tournaments/single.html', {'tournament': tournament, 'teams': teams,'sponsors': sponsors,
-                                                       'rozhodci': rozhodci,'permitted': permitted,
+
+    return render(request, 'tournaments/single.html', {'tournament': tournament, 'teams': teams, 'sponsors': sponsors,
+                                                       'rozhodci': rozhodci, 'permitted': permitted,
                                                        'permitted_add_team': permitted_add_team,
                                                        'permitted_add_sponsor': permitted_add_sponsor,
-                                                       'add_new_team': add_new_team, 'add_new_sponsor': add_new_sponsor})
-    
+                                                       'add_new_team': add_new_team,
+                                                       'add_new_sponsor': add_new_sponsor})
+
+
 def index(request):
     front = Tournament.objects.all()
     paginator = Paginator(front, 10)
-    page = request.GET.get('page')  
+    page = request.GET.get('page')
     content = paginator.get_page(page)
-    return render(request, 'tournaments/index.html', {'front':content})
+    return render(request, 'tournaments/index.html', {'front': content})
+
 
 @login_required
 def your_tournaments(request):
-
     user = get_object_or_404(User, pk=request.user.id)
     tournaments = Tournament.objects.all()
     users_tournaments = []
@@ -118,4 +123,28 @@ def your_tournaments(request):
     page = request.GET.get('page')
     content = paginator.get_page(page)
     # front = Team.objects.filter(Q(managers=user), Q(players__in=[user]))  # vem tymy kde to ridi nebo v nich hraje
-    return render(request, 'tournaments/index.html', {'front':content})
+    return render(request, 'tournaments/index.html', {'front': content})
+
+
+def start_tournament(request, id):
+    # FIXME sprav to pico
+    tournament = get_object_or_404(Tournament, pk=id)
+    teams = tournament.teams.all()
+    player_count = teams.count()
+    if teams.count() < 2:
+        messages.warning(request, 'Wrong team count: ' + str(teams.count()) + '.')
+        return render(request, 'tournaments/index.html', {'tournament': tournament})
+    else:
+        while player_count > 1:
+            if player_count % 2 != 0:
+                messages.warning(request, 'Wrong number of teams. Must be power of two. ')
+                return single(request, id)
+            player_count /= 2
+
+    for t in teams:
+        players = t.players.all()
+        if players.count() != tournament.player_count:
+            messages.warning(request, 'Wrong player count in team ' + t.name + ' .')
+            return render(request, 'tournaments/index.html', {'tournament': tournament})
+
+    return render(request, 'tournaments/match_schedule.html', {'tournament': tournament})
